18152a2963d61eb98a3b4957d507f6471c4f1122
Rémi Tribut <remi.tribut@gmail.com>
2026-04-08 12:36:31 +0200

    chabe

--- utils/constantes.py
+++ utils/constantes.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-MUR_GAUCHE = (0,0)
-MUR_DROIT = (0,0)
-SOL = (0,0)
-VMAX = None
-=======
 MUR_GAUCHE = [[0,0],[20,780]]
 MUR_DROIT = [[780,0],[800,780]]
 SOL = [[0,780],[800,800]]
->>>>>>> 119681b377b39437bd149a65db749cef68e4d9e6
